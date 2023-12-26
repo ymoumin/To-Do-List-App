b5502a3b062fc96e4a194457401af252188b388e
ymoumin <ymoum044@uottawa.ca>
2023-12-26 18:26:15 -0500

    back end fix

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_URL}));
+app.use(cors({origin: true}));
 app.use(bodyParser.json());
 
 const db = require("./models");
