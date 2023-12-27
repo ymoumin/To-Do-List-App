f57e2eaf47a796751238bb86f6c704ab75ba47a8
ymoumin <ymoum044@uottawa.ca>
2023-12-27 02:45:32 -0500

    proxy

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors({
     // (like mobile apps or curl requests)
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
+      const msg = 'The CORS policy for this site does not ' +
         'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
