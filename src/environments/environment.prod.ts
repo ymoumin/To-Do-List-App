1b5e04d34d4bdaac5f0509fe67efaa8dbd540fc5
ymoumin <ymoum044@uottawa.ca>
2023-12-27 20:36:33 -0500

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'https://your-efficient-task-integrated-interface.onrender.com'
+    apiURL: 'https://your-efficient-task-integrated-interface.onrender.com/'
   };
